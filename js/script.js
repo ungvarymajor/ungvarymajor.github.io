2b61f902652ff6cad0ff5ea975310776270b32b4
Bernadett Ungvary-Major <ungvary.major@gmail.com>
2025-04-15 13:52:06 +0200

    Vizsgamunka első verziója

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    $('nav a').on('click', function() {
+    $('button').on('click', function() {
         //Current class assignment
-        $('nav li.current').removeClass('current');
+        $('button.current').removeClass('current');
         $(this).parent().addClass('current');
 
         //Set heading text
@@ -11,7 +11,7 @@ $(document).ready(function(){
         var category = $(this).text().toLowerCase().replace(' ', '-');
 
         //Remove hidden class if 'referenciánink' is selected
-        if(category == 'referenciáink') {
+        if(category == 'modern') {
             $('ul#gallery li:hidden').fadeIn('slow').removeClass('hidden');
         } else {
             $('ul#gallery li').each(function() {
@@ -25,4 +25,43 @@ $(document).ready(function(){
         //Stop link behaviour
         return false;
     });
-});
+
+
+    $('ul#gallery li').on('mouseenter', function(){
+        //Get data attribure values
+        var title = $(this).children().data('title');
+        var desc = $(this).children().data('desc');
+
+        //Validation
+        if(desc == null) {
+            desc = 'Klikkeljen a nagyításért.';
+        }
+
+        if(title == null) {
+            title = '';
+        }
+        //Create overlay div
+        $(this).append('<div class="overlay"></div>');
+
+        //Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //Add html to overlay
+        overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+        //Fade in the overlay
+        overlay.fadeIn(800);
+    });
+
+        //Mouseleave overlay
+        $('ul#gallery li').on('mouseleave', function() {
+        //Create overlay div
+        $(this).append('<div class="overlay"></div>');
+
+        //Get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //Fade out overlay
+        overlay.fadeOut(500);
+
+        });
