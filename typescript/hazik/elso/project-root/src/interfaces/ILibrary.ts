16791aeee7cd777338a8dcd47aad9ec814ec80b2
Bernadett Ungvary-Major <ungvary.major@gmail.com>
2026-01-25 18:03:11 +0100

    deleted typescript hazi folder

--- typescript/hazik/elso/project-root/src/interfaces/ILibrary.ts
+++
@@ -1,8 +0,0 @@
-import { Book } from "../classes/book";
-
-export interface ILibrary {
-  addBook(book: Book): void;
-  removeBook(id: number): boolean;
-  findBookById(id: number): Book | undefined;
-  listAllBooks(): Book[];
-}
